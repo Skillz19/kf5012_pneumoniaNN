b3e1a5edfaf8a71347fa0d41266320c2eb955032
chidera7@live.com <skillz@SKILLZ-PC>
2023-05-05 16:00:15 +0100

    Added save model

--- model.py
+++ model.py
@@ -46,9 +46,141 @@ class Network:
         elif self.base_type == Models.ENUM_EFFICIENTNETV2:
             preprocess_input, base_model = self.load_efficientnetv2()
             return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_VGG16:
+            preprocess_input, base_model = self.load_vgg16()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_VGG19:
+            preprocess_input, base_model = self.load_vgg19()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_MOBILENET:
+            preprocess_input, base_model = self.load_mobilenet()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_MOBILENETV2:
+            preprocess_input, base_model = self.load_mobilenetv2()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_DENSENET121:
+            preprocess_input, base_model = self.load_densenet121()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_DENSENET169:
+            preprocess_input, base_model = self.load_densenet169()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_DENSENET201:
+            preprocess_input, base_model = self.load_densenet201()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_NASNETLARGE:
+            preprocess_input, base_model = self.load_nasnetlarge()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_NASNETMOBILE:
+            preprocess_input, base_model = self.load_nasnetmobile()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_XCEPTION:
+            preprocess_input, base_model = self.load_xception()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_INCEPTIONRESNETV2:
+            preprocess_input, base_model = self.load_inceptionresnetv2()
+            return preprocess_input, base_model
+        elif self.base_type == Models.ENUM_CONVNET:
+            preprocess_input, base_model = self.load_convnet()
+            return preprocess_input, base_model
         else:
             print('Invalid base model type')
             exit(-1)
+
+    def load_convnet(self):
+        preprocess_input = tf.keras.applications.vgg16.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.VGG16(input_shape=self.input_shape,
+                                                 include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_nasnetmobile(self):
+        preprocess_input = tf.keras.applications.nasnet.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.NASNetMobile(input_shape=self.input_shape,
+                                                        include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_xception(self):
+        preprocess_input = tf.keras.applications.xception.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.Xception(input_shape=self.input_shape,
+                                                    include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_inceptionresnetv2(self):
+        preprocess_input = tf.keras.applications.inception_resnet_v2.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.InceptionResNetV2(input_shape=self.input_shape,
+                                                             include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_vgg19(self):
+        preprocess_input = tf.keras.applications.vgg19.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.VGG19(input_shape=self.input_shape,
+                                                 include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_mobilenet(self):
+        preprocess_input = tf.keras.applications.mobilenet.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.MobileNet(input_shape=self.input_shape,
+                                                     include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_mobilenetv2(self):
+        preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.MobileNetV2(input_shape=self.input_shape,
+                                                       include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_densenet121(self):
+        preprocess_input = tf.keras.applications.densenet.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.DenseNet121(input_shape=self.input_shape,
+                                                       include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_densenet169(self):
+        preprocess_input = tf.keras.applications.densenet.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.DenseNet169(input_shape=self.input_shape,
+                                                       include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_densenet201(self):
+        preprocess_input = tf.keras.applications.densenet.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.DenseNet201(input_shape=self.input_shape,
+                                                       include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_nasnetlarge(self):
+        preprocess_input = tf.keras.applications.nasnet.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.NASNetLarge(input_shape=self.input_shape,
+                                                       include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
+
+    def load_vgg16(self):
+        preprocess_input = tf.keras.applications.vgg16.preprocess_input
+        # load the model
+        base_model = tf.keras.applications.VGG16(input_shape=self.input_shape,
+                                                 include_top=self.include_top, weights=self.weights_src)
+        base_model.trainable = self.trainable_base
+        return preprocess_input, base_model
 
     def load_efficientnetv2(self):
         preprocess_input = tf.keras.applications.efficientnet.preprocess_input
@@ -102,6 +234,22 @@ class Network:
         model = Flatten()(model)
         model = Dense(100)(model)
         model = Dropout(0.3)(model)
+        return model
+
+    @staticmethod
+    def add_custom_layers2(model):
+        model = Conv2D(64, (3, 3), activation='relu', padding='valid', strides=2, dilation_rate=(1, 1))(model)
+        model = BatchNormalization()(model)
+        model = Conv2D(32, (3, 3), activation='relu', padding='valid', strides=2, dilation_rate=(1, 1))(model)
+        model = BatchNormalization()(model)
+        model = Conv2D(256, (3, 3), activation='relu', padding='valid', strides=2, dilation_rate=(1, 1))(model)
+        model = BatchNormalization()(model)
+        model = Conv2D(512, (3, 3), activation='relu', padding='valid', strides=2, dilation_rate=(1, 1))(model)
+        model = BatchNormalization()(model)
+        model = Flatten()(model)
+        model = Dense(100)(model)
+        model = Dropout(0.3)(model)
+
         return model
 
     def add_output_layer(self, model, inputs):
